719b60673b6abdeaad14eccd7c1829da47096408
Ebube Aso <aso.ebube1@outlook.com>
2020-09-23 02:13:38 -0400

    Started up the database model

--- python-work/FlaskWork/FlaskProjectB/app.py
+++ python-work/FlaskWork/FlaskProjectB/app.py
@@ -8,18 +8,41 @@ with user data. However, a user can only have access to the data with an
 authorization token that gets generated when they login. They will also be
 able to register as well on this application.
 """
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, render_template, jsonify, redirect, url_for
 #import sqlite3
-from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Resource, Api
 from flask_jwt import JWT, jwt_required
 from datetime import timedelta
 from app_security import authenticate, identity
+from flask_sqlalchemy import SQLAlchemy
 #initialize the application
 app = Flask(__name__)
 api = Api(app) #allows us to make API resources
 app.secret_key = "EbubeAso"
 jwt = JWT(app, authenticate, identity)
+
 #config the JWT token to expire at a later time (in seconds)
 #this is also why I imported timedelta from datetime
 app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=1200)
+#set up SQLAlchemy
+path = """/home/pi/Documents/IT-coding-work/python-work/FlaskWork/
+FlaskProjectB/People.db"""
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////"+path
+#turns off the flask SQLAlchemy tracker to save resources
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+db = SQLAlchemy(app)
+
+#making a model to represent the database
+class Data(db.Model):
+    #tells which table to look at
+    __tablename__ = 'Employees'
+    first_name = db.Column(db.String(40))
+    last_name = db.Column(db.String(40))
+    age = db.Column(db.Integer)
+    employeeID = db.Column(db.Integer, primary_key=True)
+    role = db.Column(db.String(40))
+    salary = db.Column(db.Integer)
+    
+
+if __name__ == "__main__":
+    app.run()
