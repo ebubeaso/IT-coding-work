eeb61263318bd3ed25462732fad168c699c76a02
Ebube Aso <aso.ebube1@outlook.com>
2020-09-22 23:00:50 -0400

    moving around some files for organization

--- python-work/FlaskWork/FlaskProjectB/app_security.py
+++ python-work/FlaskWork/FlaskProjectB/app_security.py
@@ -6,45 +6,10 @@ authentication purposes. It will find the username and the id of the user so
 that it can make a JWT authorization token for the logged in user to use the APIs
 in this flask application
 """
-import sqlite3
 from werkzeug.security import safe_str_cmp
+#this is importing from the user.py file
+from user import User
 
-class User:
-    def __init__(self, _id, username, password):
-        self.id = _id
-        self.username = username
-        self.password = password
-
-    #meant to find the username of the user for authentication
-    @classmethod
-    def find_username(cls, username):
-        conn = sqlite3.connect('UserData.db')
-        cur = conn.cursor()
-        sql = "select * from Users where username=?"
-        cur.execute(sql, (username,))
-        row = cur.fetchone()
-        #check if row has any data from the query
-        if row:
-        #a tuple that uses its elements as arguments for the User object
-            user = cls(*row)
-        else:
-            user = None
-        conn.close()
-        return user
-
-    @classmethod
-    def find_by_id(cls, _id):
-        conn = sqlite3.connect('UserData.db')
-        with conn:
-            cur = conn.cursor()
-            sql = "select * from Users where id=?"
-            cur.execute(sql, (_id,))
-            row = cur.fetchone()
-            if row:
-                user = cls(*row) #uses the row tuple as arguments for User object
-            else:
-                user = None
-        return user
 """
 Takes in a username and password. This is used to authenticate the user
 and it will see if the credentials are correct from the query
