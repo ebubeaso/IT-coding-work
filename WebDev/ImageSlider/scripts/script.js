3921607a936758cd8eef44d0ef9d19b69f371e12
Ebube Aso <aso.ebube1@outlook.com>
2020-09-23 21:08:01 -0400

    removed the unused sliderDiv variable

--- WebDev/ImageSlider/scripts/script.js
+++ WebDev/ImageSlider/scripts/script.js
@@ -1,11 +1,10 @@
 //This function is made to slide the images automatically
 $(function() {
     let width = 450 //the width of the images
-    let animateSpeed = 1500;
+    let animateSpeed = 1000;
     let pause = 3000;
-    let currentImage = 1;
+    let currentImage = 1; //represents the first image on the slide
     //Cache the DOM elements
-    let $sliderDiv = $("#slider");
     let $slides = $("#the-slides");
     let $images = $slides.find(".image-slide");
 
