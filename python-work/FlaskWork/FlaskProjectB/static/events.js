70beacd5a33767863704a6340ff20a436f02235e
Ebube Aso <aso.ebube1@outlook.com>
2020-09-29 10:25:02 -0400

    modified the header to look better

--- python-work/FlaskWork/FlaskProjectB/static/events.js
+++ python-work/FlaskWork/FlaskProjectB/static/events.js
@@ -4,6 +4,7 @@ $(function() {
     $("#title").hide().slideDown(700);
     $("#menu").hide().slideDown(800);
     $("#menu2").hide().slideDown(800);
+    $("header").hide().fadeIn(600);
     $("h2").hide().fadeIn(2000);
     $(".explanation").hide().fadeIn(2500);
     $("#token").hide().fadeIn(2500);
