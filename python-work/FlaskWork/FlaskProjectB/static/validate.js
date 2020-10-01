74e0294200c3ee3c53a1d90846262fe60a48fd9f
Ebube Aso <aso.ebube1@outlook.com>
2020-10-01 08:22:35 -0400

    added a form validation script for registration

---
+++ python-work/FlaskWork/FlaskProjectB/static/validate.js
@@ -0,0 +1,48 @@
+//This is to validate the password entry for a registering user
+$(function() {
+    $("#signup-submit").on("click", function(){
+        let formValid = true;
+        let validUser = true;
+        let validPassword = true;
+        let inputUsername = $("#user").val();
+        let inputPassword = $("#passwd").val();
+        //counts the number of uppercase letters
+        let upperCaseCount = 0;
+        //special characters to make password more secure
+        let chars = ["!", "?", "$", "@", "%", "^", "&"];
+        let charsCount = 0;
+        let numberCount = 0;
+
+        if (inputUsername.length == 0) {
+            validUser = false;
+            $("#userError").removeClass("hidden");
+        } else {
+            $("#userError").addClass("hidden");
+            validUser = true;
+        }
+        //made some loops to look for capital letters or special characters
+        for (let i=0; i <= inputPassword.length; i++) {
+            let letter = inputPassword.charAt(i);
+            let character = inputPassword.charAt(i);
+            let number = inputPassword.charAt(i);
+            if (letter == letter.toUpperCase()) {
+                upperCaseCount++;
+            }
+            if(isNaN(parseInt(number)) == false) {
+                numberCount++;
+            }
+            if (chars.includes(character)) {
+                charsCount++;
+            }
+        };
+        if (inputPassword.length < 0 || upperCaseCount == 0 || numberCount == 0 || charsCount == 0) {
+            validPassword = false;
+            $("#passwordError").removeClass("hidden2");
+        } else {
+            ("#passwordError").addClass("hidden2");
+            validPassword = true;
+        }
+        formValid = (validUser && validPassword);
+        return formValid;
+    });
+});
