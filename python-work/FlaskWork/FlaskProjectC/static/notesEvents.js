8567ead26974cac67bbbc1a644ab910c786fc208
Ebube Aso <aso.ebube1@outlook.com>
2020-11-29 20:58:44 -0500

    Can delete notes from the web application

--- python-work/FlaskWork/FlaskProjectC/static/notesEvents.js
+++ python-work/FlaskWork/FlaskProjectC/static/notesEvents.js
@@ -1,14 +1,4 @@
 // Handling events that happen in the notes page
-
-// made this function to select the entry ID of a note
-// let myTable = document.getElementById("table-body");
-// myTable.addEventListener('click', event => {
-//     for (let i=0; i< myTable.length; i++) {
-//         for (let j=0; j < myTable.rows[i].cells.length; j++) {
-//             if(event.target.className === 'modify') {}
-//         };
-//     };
-// })
     
 //made this function for handling PUT ssrequests
 let url = '';
@@ -16,23 +6,33 @@ function updateNoteEntry(u) {
     console.log(u);
     let xhr = new XMLHttpRequest();
     xhr.open('PUT', u, true);
-    //xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
     let form = document.getElementById("change-note-form");
     let formData = new FormData(form);
     xhr.send(formData);
+};
+
+/* made this variable to help with refreshing tha page after 
+doing a DELETE request (it is a nice workaround) */
+let notesLocation = window.location;
+
+// made this function to handle DELETE requests
+function deleteNoteEntry(u) {
+    console.log(u);
+    let xhr = new XMLHttpRequest();
+    xhr.open('DELETE', u, true);
+    xhr.send();
 }
 
 $(function() {
-    //for adding a new note
+    // for adding a new note
     let myTable = $("#table-body");
     let entryForm = $("#new-note-form");
     let popupBackground = $("#add-note-popup");
     let closeAddButton = $("#close-add"); // closes popup screen
-    //for changing a current note
+    // for changing a current note
     let updateForm = $("#change-note-form");
     let updatePopupScreen = $("#change-note-popup");
     let closeChangeButton = $("#close-update");
-    //let submitUpdate = $("#submit-update");
 
     entryForm.hide();
     updateForm.hide();
@@ -49,7 +49,7 @@ $(function() {
         popupBackground.fadeOut(600);
     });
 
-    //These two events show and hide the popup for updating a note
+    // These two events show and hide the popup for updating a note
     myTable.on('click', '.modify a', (event) => {
         event.preventDefault();
         updatePopupScreen.css("display", "flex").fadeIn(300);
@@ -61,6 +61,7 @@ $(function() {
         updateForm.slideUp(300);
         updatePopupScreen.fadeOut(600);
     });
+    // This event is what leads to making the PUT request
     entryForm.on('submit', () => {return true;});
     $("#submit-update").on('click', (event) => {
         updateNoteEntry(url);
@@ -68,4 +69,13 @@ $(function() {
         alert('The note entry has been updated!');
         location.reload(); //refreshes the webpage
     } );
+    // This event is what leads to the DELETE request (based off of PUT request)
+    myTable.on('click', '.delete a', (event) => {
+        url = event.target.href;
+        confirm("Are you sure you want to delete this note? Click 'OK' if you are.");
+        deleteNoteEntry(url);
+        alert('The note entry has been deleted!');
+        window.location = notesLocation;
+        event.preventDefault();
+    })
 });
