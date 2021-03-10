bfb3708c5b7bb46aff3f608efa23299e16631a01
Ebube Aso <aso.ebube1@outlook.com>
2021-03-09 22:14:11 -0500

    added some comments for documentation purposes

--- nodejs-work/ReactNode/ReactPilot/frontend/src/App.js
+++ nodejs-work/ReactNode/ReactPilot/frontend/src/App.js
@@ -3,7 +3,7 @@
 class TheButton extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {showTable: false};
+        this.state = {showTable: false}; // for conditional rendering
         // binds the buttonClick function to this state (makes the component stateful)
         this.buttonClick = this.buttonClick.bind(this);
     };
@@ -12,20 +12,27 @@ class TheButton extends React.Component {
         this.setState({showTable: true});
     };
     render() {
+        // renders the button
         return (
             <div>
                 <button className="Click" onClick={this.buttonClick}>Click Me</button>
-                {this.state.showTable ? <DataTable /> : null}
+                {/* {This ternary operator is used to determine whether to show the table
+                    or not, depending if the button is clicked} */}
+                {this.state.showTable ? <DataTable /> : null} 
             </div>
         );
     }
 };
+// this component shows the data from the MariaDB database
 class DataTable extends React.Component {
     constructor(props) {
         super(props);
         this.state = {cars: []};
     }
     // Get the luxury car data from the Node.js backend
+    /* First it gets the response, then it takes the data and changes the state of cars
+    to an array of objects that represent each car from the data query. The err just catches
+    any errors */
     componentDidMount() {
         fetch("http://localhost:3001/table")
             .then(response => response.json())
@@ -33,10 +40,12 @@ class DataTable extends React.Component {
             .then(err => {throw err;})
     };
     render() {
+        // This is responsible for rendering the table
         return (
         <div className="MyTable">
         <h1 className="Title">Luxury Cars Table</h1>
         <table>
+            {/* The thead is for the column headers */}
             <thead>
             <tr>
                 <th>Brand Name</th>
@@ -46,6 +55,9 @@ class DataTable extends React.Component {
             </tr>
             </thead>
             <tbody>
+            {/* This uses the map function, which iterates through the array of objects
+            and maps a value in each object as a value for a HTML element. It needs a key, a
+            unique identifier, so I used the id of the data since it will be the unique key */}
             {this.state.cars.map((car) => (
                 <tr key={car.id}>
                     <td>{car.BrandName}</td>
