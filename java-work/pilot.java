57484d50b4877ee7eddfca4a9247ebac46915d83
Ebube Aso <aso.ebube1@outlook.com>
2021-04-04 17:21:48 -0400

    first java program code

---
+++ java-work/pilot.java
@@ -0,0 +1,7 @@
+public class PilotCode {
+    public static void main(String[] args) {
+        int number = 25;
+        String name = "Pierre";
+        System.out.println("My name is " + name + " and I am " + number + " years old");
+    }
+}
