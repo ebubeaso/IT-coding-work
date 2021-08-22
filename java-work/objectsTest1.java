7e90e04ba1313a5d07e867e32a26c868bf66bfe2
Ebube Aso <aso.ebube1@outlook.com>
2021-08-22 16:31:46 -0400

    few project work

---
+++ java-work/objectsTest1.java
@@ -0,0 +1,9 @@
+// This is simply being used to test making objects in Java
+public class objectsTest1 {
+    public static void main(String[] args) {
+        System.out.println("Howdy");
+    }
+    static class Car {
+        String name;
+    }
+}
